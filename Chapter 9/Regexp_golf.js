601e83d512813236f769d29a2b34b78a597e6383
Eduardo de Rivero <eduardo.derivero@valigo.org>
2021-01-16 21:30:22 -0500

    chapter 12

--- Chapter 9/Regexp_golf.js
+++ Chapter 9/Regexp_golf.js
@@ -1,39 +1,42 @@
-verify(/ca[rt]/,
-    ["my car", "bad cats"],
-    ["camper", "high art"]);
+verify(/ca[rt]/, ["my car", "bad cats"], ["camper", "high art"]);
 
-verify(/pr?op/,
-    ["pop culture", "mad props"],
-    ["plop", "prrrop"]);
+verify(/pr?op/, ["pop culture", "mad props"], ["plop", "prrrop"]);
 
-    verify(/ferr(et|y|ari)/,
-    ["ferret", "ferry", "ferrari"],
-    ["ferrum", "transfer A"]);
+verify(
+  /ferr(et|y|ari)/,
+  ["ferret", "ferry", "ferrari"],
+  ["ferrum", "transfer A"]
+);
 
-verify(/ious\b/,
-    ["how delicious", "spacious room"],
-    ["ruinous", "consciousness"]);
+verify(
+  /ious\b/,
+  ["how delicious", "spacious room"],
+  ["ruinous", "consciousness"]
+);
 
-verify(/\s[.,:;]/,
-    ["bad punctuation ."],
-    ["escape the dot"]);
+verify(/\s[.,:;]/, ["bad punctuation ."], ["escape the dot"]);
 
-verify(/\w{7}/,
-    ["Siebentausenddreihundertzweiundzwanzig"],
-    ["no", "three small words"]);
+verify(
+  /\w{7}/,
+  ["Siebentausenddreihundertzweiundzwanzig"],
+  ["no", "three small words"]
+);
 
-verify(/\b[^\We]+\b/i,
-    ["red platypus", "wobbling nest"],
-    ["earth bed", "learning ape", "BEET"]);
-
+verify(
+  /\b[^\We]+\b/i,
+  ["red platypus", "wobbling nest"],
+  ["earth bed", "learning ape", "BEET"]
+);
 
 function verify(regexp, yes, no) {
-// Ignore unfinished exercises
-if (regexp.source == "...") return;
-for (let str of yes) if (!regexp.test(str)) {
- console.log(`Failure to match '${str}'`);
-}
-for (let str of no) if (regexp.test(str)) {
- console.log(`Unexpected match for '${str}'`);
-}
+  // Ignore unfinished exercises
+  if (regexp.source == "...") return;
+  for (let str of yes)
+    if (!regexp.test(str)) {
+      console.log(`Failure to match '${str}'`);
+    }
+  for (let str of no)
+    if (regexp.test(str)) {
+      console.log(`Unexpected match for '${str}'`);
+    }
 }
